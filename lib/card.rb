4b7feba31ecad4d3d5c395d90fd2992c3075c146
Dianna Tingg <dtingg@gmail.com>
2019-08-26 08:28:59 -0700

    validates card value and suit

--- lib/card.rb
+++ lib/card.rb
@@ -1,18 +1,35 @@
-
-# card.rb
-
 class Card
   attr_reader :value, :suit
-
+  
   def initialize(value, suit)
-    @value = value
-    @suit = suit
+    valid_values = (1..13).to_a
+    valid_suits = [:hearts, :spades, :clubs, :diamonds]
     
-
+    if valid_values.include?(value)
+      @value = value
+    else
+      raise ArgumentError.new("That is not a valid value.")
+    end
+    
+    if valid_suits.include?(suit)
+      @suit = suit
+    else
+      raise ArgumentError.new("That is not a valid suit.")
+    end
   end
-
+  
+  def convert_special_cards(card_number)
+    special_cards = { 1 => "Ace", 11 => "Jack", 12 => "Queen", 13 => "King" }
+    
+    if special_cards.has_key?(card_number)
+      return special_cards[card_number]
+    else
+      return card_number
+    end
+  end
+  
   def to_s
-    return "#{value} of #{suit.to_s}"
+    converted_value = convert_special_cards(value)
+    return "#{converted_value} of #{suit}"
   end
-
 end
