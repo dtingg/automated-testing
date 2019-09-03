22c739f0989e6183e8cf1197192dc0b5fe3dd2fd
Dianna Tingg <dtingg@gmail.com>
2019-09-03 10:04:47 -0700

    added simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,11 @@
+require "simplecov"
+SimpleCov.start do
+  add_filter "test/"
+end
+
 require 'minitest/autorun'
 require 'minitest/reporters'
 require_relative '../lib/card'
 require_relative '../lib/deck'
-
-
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
